053e1bfe173ce11afd6d24dc2b1c78bddb9752e1
md shakil <mdshakil6539@gmail.com>
2026-05-19 16:39:51 +0600

    env file fix

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import authRoutes from './routes/authRoutes.js';
+import projectRoutes from './routes/projectRoutes.js';
+import contactRoutes from './routes/contactRoutes.js';
+
+if (process.env.NODE_ENV !== 'production') {
+  const dotenv = await import('dotenv');
+  dotenv.config();
+}
 
 const app = express();
 app.use(cors({ origin: process.env.CLIENT_URL }));
 app.use(express.json());
 
-// Routes
-import projectRoutes from './routes/projectRoutes.js';
-import contactRoutes from './routes/contactRoutes.js';
 app.use('/api/projects', projectRoutes);
 app.use('/api/contact', contactRoutes);
 app.use('/api/auth', authRoutes);
@@ -20,5 +22,5 @@ mongoose.connect(process.env.MONGO_URI, {
   serverSelectionTimeoutMS: 5000,
   family: 4
 })
-  .then(() => app.listen(5000, () => console.log('Server running on port 5000')))
+  .then(() => app.listen(process.env.PORT || 5000, () => console.log('Server running on port 5000')))
   .catch(err => console.log(err.message));
